f3cb1e5bea9bb17d8154cb653e1a0af44c57804c
sem <sembenavente@upeu.edu.pe>
2022-06-20 19:12:42 -0500

    version

---
+++ svm/code.py
@@ -0,0 +1,12 @@
+from ctypes.wintypes import HLOCAL
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC, SVC, LinearSVR, SVR
+
+print('Hola')
